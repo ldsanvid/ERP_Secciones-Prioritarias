66dfdacca3b9c9e23b91cd353aca85c96b0be76e
Luis Daniel Santiago Vidargas <42190833+ldsanvid@users.noreply.github.com>
2022-04-04 16:44:22 -0500

    Update grafpuebla.js

--- grafpuebla.js
+++ grafpuebla.js
@@ -1,22 +1,8 @@
-var votototalERPcapital = {
+var votototalERP = {
     x: ['DIC20', 'FEB21', 'MAR21', 'ABR21', 'MAY21', 'JUN21','SEP21','ENE22'],
-    y: [388977, 379531, 377674, 378230, 353389, 360665, , ],
+    y: [388977, 379531, 377674, 378230, 353389, 360665, 909339, 928561],
     mode: 'lines',
-    name: "Voto total de ERP en Puebla Capital",
-    line:{
-      dash: 'dashdot',
-      width: 4
-    },
-    marker: {
-      color: 'rgba(1, 5, 226, 0.8)'
-    }
-  };
-  
-  var votototalERPestado = {
-    x: ['DIC20', 'FEB21', 'MAR21', 'ABR21', 'MAY21', 'JUN21','SEP21','ENE22'],
-    y: [ , , , , , , 909339, 9285651],
-    mode: 'lines',
-    name: "Voto total de ERP en Puebla Estado",
+    name: "Voto Total de ERP",
     line:{
       dash: 'dashdot',
       width: 4
@@ -26,11 +12,12 @@ var votototalERPcapital = {
     }
   };
   
-  var votototalERPcapital = {
+
+  var votoduroERP = {
     x: ['DIC20', 'FEB21', 'MAR21', 'ABR21', 'MAY21', 'JUN21','SEP21','ENE22'],
-    y: [202028, 205405, 200977, 198172, 194786, , ],
+    y: [202028, 205405, 200977, 198172, 190883, 194786, 398433, 476191],
     mode: 'lines',
-    name: "Voto Duro de ERP en Puebla Capital",
+    name: "Voto Duro de ERP",
     line:{
       dash: 'solid',
       width: 4
@@ -40,24 +27,15 @@ var votototalERPcapital = {
     }
   };
   
-  var votoduroERPestado = {
-    x: ['DIC20', 'FEB21', 'MAR21', 'ABR21', 'MAY21', 'JUN21','SEP21','ENE22'],
-    y: [ , , , , , , 398433, 476191],
-    mode: 'lines',
-    name: "Voto Duro de ERP en Puebla Estado",
-    line:{
-      dash: 'solid',
-      width: 4
-    },
-    marker: {
-      color: 'rgba(0, 167, 255, 0.8)'
-    }
-  };
+
   
-  var radarespuebla = [votototalERPcapital, votototalERPestado, votototalERPcapital, votoduroERPestado];
+  var radarespuebla = [votototalERP, votoduroERP];
   
   var grafradares = {
-    title:'Estimación de votos', yaxis:{title:"Miles de votos"}
+    title:'Estimación de votos',
+    yaxis: {range:[10000, 1000000],
+        title: "Miles de votos"
+    }
   };
   
   Plotly.newPlot('Radar Puebla', radarespuebla, grafradares)
